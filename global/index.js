e8fd40a7ebaa6f0691088bae2d3c0856d4c3c43a
Yan <yantedesco@gmail.com>
2021-05-23 20:45:30 -0300

    hamburger button

--- global/index.js
+++ global/index.js
@@ -2,7 +2,21 @@
 
 const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
 const navLinks = document.querySelectorAll(".nav-link");
+
+menuBtn.onclick = () => {
+	navbar.classList.add("show");
+	menuBtn.classList.add("hide");
+	body.classList.add("disabled");
+};
+
+cancelBtn.onclick = () => {
+	body.classList.remove("disabled");
+	navbar.classList.remove("show");
+	menuBtn.classList.remove("hide");
+};
 
 window.onscroll = () => {
 	this.scrollY > 20
@@ -10,41 +24,10 @@ window.onscroll = () => {
 		: navbar.classList.remove("sticky");
 };
 
-// Get the modal
-var modal = document.querySelectorAll(".modal");
-
-// Get the button that opens the modal
-var btn = document.querySelectorAll(".myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-for (var i = 0; i < btn.length; i++) {
-	btn[i].onclick = (e) => {
-		e.preventDefault();
-		modal = document.querySelector(e.target.getAttribute("href"));
-		modal.style.display = "block";
-	};
-}
-
-// When the user clicks on <span> (x), close the modal
-for (var i = 0; i < span.length; i++) {
-	span[i].onclick = () => {
-		for (var index in modal) {
-			console.log(typeof modal[index].style);
-			if (typeof modal[index].style !== "undefined")
-				modal[index].style.display = "none";
-		}
-	};
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-	if (event.target.classList.contains("modal")) {
-		for (var index in modal) {
-			if (typeof modal[index].style !== "undefined")
-				modal[index].style.display = "none";
-		}
-	}
-};
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		body.classList.remove("disabled");
+		navbar.classList.remove("show");
+		menuBtn.classList.remove("hide");
+	});
+});
